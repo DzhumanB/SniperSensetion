9f82d2e7d4511ed7c885614e020f21867bab6ed7
DzhumanB <bohdan1995@live.it>
2018-07-25 10:32:31 +0200

    Network Manager
    - Configurazione del Network manager
    - Importazione degli Standard assets per il FPS
    - Build del gioco per testare il multiplayer

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(ConfigurableJoint))]
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5.0f;
+    [SerializeField]
+    private float lookSensitivity = 3.0f;
+
+
+    [SerializeField]
+    private float thrusterForceCoef = 10.0f;
+
+    [Header("Sprint settings:")]
+    //[SerializeField]
+    //private JointDriveMode jointMode = JointDriveMode.Position;
+    //[SerializeField]
+    //private float jointSprint = 5.0f;
+    //[SerializeField]
+    //private float jointMaxForce = 2.0f;
+
+    private PlayerMotor motor;
+    //private ConfigurableJoint joint;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        //joint = GetComponent<ConfigurableJoint>();
+    }
+
+    private void Update()
+    {
+        //Calculate movement velocity as a 3D vector
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * zMov;
+
+        //Final move vector
+        Vector3 velocity = (movHorizontal + movVertical).normalized * speed;
+
+        //Apply movement
+        motor.Move(velocity);
+
+        //Calculate rotation as a 3D vector (turning around)
+        float yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * lookSensitivity;
+
+        //Apply  rotation
+        motor.Rotate(rotation);
+
+        //Calculate camera rotation as a 3D vector (turning around)
+        float xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 cameraRotation = new Vector3(xRot, 0f, 0f) * lookSensitivity;
+
+        //Apply  rotation
+        motor.RotateCamera(cameraRotation);
+
+
+        //Calculate the thrusterforce based on player input
+        Vector3 thrusterForce = Vector3.zero;
+        if (Input.GetButton("Jump"))
+        {
+            thrusterForce = Vector3.up * thrusterForceCoef;
+        }
+
+        //Apply thruster force
+        motor.ApplyThruster(thrusterForce);
+    }
+}
+
---
+++ New Unity Project/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour{
+
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+    private Vector3 thrusterForce = Vector3.zero;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Run every physics iterations
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       
+    }
+
+    //Gets a movement vector
+    public void Move(Vector3 velocityDiff)
+    {
+        velocity = velocityDiff;
+    }
+
+    //Gets a rotational vector
+    public void Rotate(Vector3 rotationDiff)
+    {
+        rotation = rotationDiff;
+    }
+
+    //Gets a rotational vector for the camera
+    public void RotateCamera(Vector3 cameraRotationDiff)
+    {
+        cameraRotation = cameraRotationDiff;
+    }
+
+        // Get force vector for our thrusters
+    public void ApplyThruster(Vector3 thrusterForceDiff)
+    {
+        thrusterForce = thrusterForceDiff;
+    }
+
+    //Perform movement based on velocity variable
+    void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.deltaTime);
+        }
+
+        if(thrusterForce != Vector3.zero)
+        {
+            rb.AddForce(thrusterForce * Time.fixedDeltaTime, ForceMode.Acceleration);
+        }
+    }
+
+    //Perform rotate based on velocity variable
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if(cam != null)
+        {
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+}
