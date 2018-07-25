095fa61882b2d4cdd6c14b13bc6b47a219acd584
DzhumanB <bohdan1995@live.it>
2018-07-25 16:47:26 +0200

    Bullet
    - Implementazione del proiettile, con i relativi componenti necessari per farli avere la balistica.
    - TODO: Risolvere il problema del collider, migliorare la balistica

---
+++ New Unity Project/Assets/GunScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+    public float damage = 100f;
+    public float range = 100f;
+
+    public Camera fpsCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
